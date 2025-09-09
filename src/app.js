59c2226e359b7baa4d0682fc17832aa287541782
Ankur Sangwan <ankursangwan@Ankurs-MacBook-Air.local>
2025-09-09 15:11:25 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express=require("express");
+
+const app=express();
+
+app.use("/hello",(req,res)=>{
+    res.send("hello from express")
+})
+
+app.use("/test",(req,res)=>{
+    res.send("hello world")
+})
+
+app.use("/wow",(req,res)=>{
+    res.send("looking good")
+})
+
+app.listen(3000,()=>{
+    console.log("server is listening on port 3000");
+});
