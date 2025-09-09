4f656c35410255c5de2385f872b5e3eb00c570fd
Ankur Sangwan <ankursangwan@Ankurs-MacBook-Air.local>
2025-09-09 16:52:33 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,6 @@ const express=require("express");
 
 const app=express();
 
-app.use("/hello",(req,res)=>{
-    res.send("hello from express")
-});
-
-app.use("/test",(req,res)=>{
-    res.send("hello world")
-});
-
 app.use("/wow",(req,res)=>{
     res.send("wow this is great!")
 });
