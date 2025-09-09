9c77ecd98f721bea8adf3cd77de43546a9827c07
Ankur Sangwan <ankursangwan@Ankurs-MacBook-Air.local>
2025-09-09 15:59:29 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ const app=express();
 
 app.use("/hello",(req,res)=>{
     res.send("hello from express")
-})
+});
 
 app.use("/test",(req,res)=>{
     res.send("hello world")
-})
+});
 
 app.use("/wow",(req,res)=>{
-    res.send("looking good")
-})
+    res.send("wow this is great!")
+});
 
 app.listen(3000,()=>{
     console.log("server is listening on port 3000");
