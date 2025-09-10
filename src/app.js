1e06d58e61ede1f419033e9d727a575a98ac1b12
Ankur2508 <ankursangwan2508@gmail.com>
2025-09-10 12:54:43 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -4,13 +4,17 @@ const app=express();
 
 //this will only handle GET call to /user
 app.get("/user",(req,res)=>{
-    console.log(req.query);
+   //console.log(req.query);
     res.send({firstName:"Ankur",lastName:"Sangwan"});
 });
 
 //this will match all types Http methods API calls to /wow
 app.use("/wow",(req,res)=>{
     res.send("wow this is great!")
+});
+app.post("/how",(req,res)=>{
+    console.log(req.query);
+    res.send("how are you?")
 });
 
 app.listen(3000,()=>{
