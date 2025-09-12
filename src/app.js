f8c272496a2bc318f0b0b3a327d07318c9ffec16
Ankur2508 <ankursangwan2508@gmail.com>
2025-09-12 17:20:42 +0530

    delete and patch api

--- src/app.js
+++ src/app.js
@@ -154,31 +154,21 @@ app.delete("/user",async(req,res)=>{
     }
 });
 
-app.patch("/user",async(req,res)=>{
-    const id=req.body.id;
-    const data=req.body;
-    try{
-        const ALLOWED_UPDATE=["firstname","age","gender"];
-        const isUpdateAllowed=Object.keys(data).every((k)=>
-            ALLOWED_UPDATE.includes(k)
-    );
-        if(!isUpdateAllowed){
-            throw new Error("invalid updates");
-        }
-        if(data?.skills.length>10){ 
-            throw new Error("skills length is more than 10");
-        }
-        const user =await User.findByIdAndUpdate(id,data,{
-            returnDocument:"after",
-            runValidators:true,
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(userId, data, {
+            returnDocument: "after",
         });
-        res.send("user data updated successfully");
+        console.log(user);
+    res.send("User updated successfully");
+    } 
+    catch (err) 
+    {
+    res.status(400).send("Something went wrong ");
     }
-    catch(err){
-        console.error("error updating user data",err);
-        res.status(400).send("error updating user data"+err.message);
-    }
-})
+});
 connectDB()
     .then(()=>{
         console.log("database is established");
